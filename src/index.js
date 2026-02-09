e39005fadee778ad7e854683f98c72184ea32ea9
hasib_inamdar <hasib.inamdar.pro@gmail.com>
2026-02-09 18:24:30 +0530

    Implemented custom async API handler, Error and response
    Created custom ApiErro for standardizing the error structure
    
    Created custom ApiResponse form standard respones
    
    Created Async Error Handler Wrapper for handling the error at one place

--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 console.log("In main index page");
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error at listining at port: ", error);
+      throw error;
+    });
 
-// const connectDB = () => {};
-// connectDB();
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-
-//     app.on("error", (error) => {
-//       console.log("Error:", error);
-//       throw error;
-//     });
-
-//     app.listen(process.env.PORT, () => {
-//       console.log(`App is listening on port ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("Error: ", error);
-//     throw error;
-//   }
-// })();
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at ${process.env.PORT} port`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connect error!", err);
+  });
