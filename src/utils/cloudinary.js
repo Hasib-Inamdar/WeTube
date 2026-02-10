1d8e1dd8e906cc1d38f1b9f44802fcd2ff5feb5c
hasib_inamdar <hasib.inamdar.pro@gmail.com>
2026-02-10 16:01:22 +0530

    Created Multer middleware for calling the upload function of cloudinary and handling localserver upload feature

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,25 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null; // If there is no localfile then return null
+
+    //Upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // If file is uploaded
+    console.log("File has been uploaded successfully ", response.url);
+    return response;
+  } catch (error) {
+    console.log("Error: ", error);
+    console.error("Cant upload data to cloudinary");
+    fs.unlinkSync(localFilePath); //Remove the locally saved file if upload operation failed
+    return null;
+    //
+  }
+};
+
+export { uploadOnCloudinary };
