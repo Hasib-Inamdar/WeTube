2dfaa67867cb66a6211a734de5251bf025d75aa0
hasib_inamdar <hasib.inamdar.pro@gmail.com>
2026-02-10 18:33:59 +0530

    Created controllers and router for the user

--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ app.use(
     credentials: true,
   }),
 );
-
 app.use(
   express.json({
     limit: "16kb",
@@ -25,4 +24,8 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+// --------------------------------------------------------------------------
+// ROUTES IMPORTING
+import userRouter from "./routes/user.routes.js";
+app.use("/api/v1/users", userRouter);
 export { app };
