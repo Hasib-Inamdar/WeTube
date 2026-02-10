973adb66f797005345017f195936e8abd1760aa2
hasib_inamdar <hasib.inamdar.pro@gmail.com>
2026-02-10 12:39:10 +0530

    Password comparision and JWT Tokens implementation
    Added custom method to check the hashed password with plain password
    
    Created JST access and refresht token functions

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,45 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new Schema({}, { timestamps: true });
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //Cloudnari video URL
+      required: true,
+    },
+    thumbnail: {
+      type: String, //Cloudnari video URL
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: number,
+      required: true,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
 
+videoSchema.plugin(mongooseAggregatePaginate);
 export const Video = mongoose.model("Video", videoSchema);
