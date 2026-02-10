1ed8a5ab24a263f476a2130a78a42fbea8ee82fc
hasib_inamdar <hasib.inamdar.pro@gmail.com>
2026-02-10 11:03:05 +0530

    Added User Model code in the user.model.js file

---
+++ src/models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //We will use cloudinary URL
+      required: true,
+    },
+    coverimage: {
+      type: String,
+      required: true,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshtoken: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
