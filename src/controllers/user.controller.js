cca102e987074d23f1f6cd89d37ee20f8205cd79
hasib_inamdar <hasib.inamdar.pro@gmail.com>
2026-02-12 13:04:06 +0530

    Tested the register user enpoint with test respone in postman. Used the file uplod middlware before calling executing the post request. Added empty field validation for user. Added existing user validation. Added validation for the file upload.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,32 @@
+import { log } from "console";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
+  const { fullname, username, email, password } = req.body;
+
+  // VALIDATIONS:
+
+  // 1. Check if any field is empty or not
+  if (
+    [fullname, username, email, password].some((field) => field?.trim() === "")
+  ) {
+    // If any one field is empty the it will print error message.
+    throw new ApiError(400, "All field are required");
+  }
+
+  // 2. Check if user already exist in DB with same username or email
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
   });
+  if (existedUser) {
+    throw new ApiError(409, "User already exist");
+  }
+
+  // 3. File upload avatar
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverimageLocalPath = req.files?.coverimage[0]?.path;
 });
 
 export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  // Middleware for uploading fiels
+  upload.fields([
+    {
+      name: "avatar", // Should be same in frontend
+      maxCount: 1,
+    },
+    {
+      name: "coverimage", // Should be same in frontend
+      maxCount: 1,
+    },
+  ]),
+  registerUser,
+);
 
 export default router;
